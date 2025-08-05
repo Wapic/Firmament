e362b95b3ee70fa5745463fc78507e230b607c3f
Linnea Gräf <nea@nea.moe>
2025-08-05 17:46:32 +0200

    fix: Prefer natural vanilla item stacks over repo item stacks

--- src/main/kotlin/util/HoveredItemStack.kt
+++ src/main/kotlin/util/HoveredItemStack.kt
@@ -6,19 +6,28 @@ import net.minecraft.item.ItemStack
 import moe.nea.firmament.mixins.accessor.AccessorHandledScreen
 import moe.nea.firmament.util.compatloader.CompatLoader
 
-interface HoveredItemStackProvider {
+interface HoveredItemStackProvider : Comparable<HoveredItemStackProvider> {
 	fun provideHoveredItemStack(screen: HandledScreen<*>): ItemStack?
+	override fun compareTo(other: HoveredItemStackProvider): Int {
+		return compareValues(this.prio, other.prio)
+	}
+
+	val prio: Int get() = 0
 
 	companion object : CompatLoader<HoveredItemStackProvider>(HoveredItemStackProvider::class)
 }
 
 @AutoService(HoveredItemStackProvider::class)
 class VanillaScreenProvider : HoveredItemStackProvider {
+
 	override fun provideHoveredItemStack(screen: HandledScreen<*>): ItemStack? {
 		screen as AccessorHandledScreen
 		val vanillaSlot = screen.focusedSlot_Firmament?.stack
 		return vanillaSlot
 	}
+
+	override val prio: Int
+		get() = -1
 }
 
 val HandledScreen<*>.focusedItemStack: ItemStack?

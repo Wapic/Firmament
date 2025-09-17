a0b82b62198b03b0cd5ef7a2b6206c543dd6db3f
Linnea Gräf <nea@nea.moe>
2025-09-17 20:42:36 +0200

    fix(web): pnpm build

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -3,10 +3,12 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), mdx()],
   redirects: {
-	"/discord": "https://discord.com/invite/64pFP94AWA",
+    "/discord": "https://discord.com/invite/64pFP94AWA",
   },
 });

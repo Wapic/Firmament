fc74db9ce60afd2c18919531f132ef2f4e56fa80
Linnea Gräf <nea@nea.moe>
2025-10-14 23:19:53 +0200

    fix(dev): scheduleStop identifier being called multiple times

--- src/main/java/moe/nea/firmament/mixins/devenv/IdentifyStopperPatch.java
+++ src/main/java/moe/nea/firmament/mixins/devenv/IdentifyStopperPatch.java
@@ -3,14 +3,19 @@ package moe.nea.firmament.mixins.devenv;
 
 import net.minecraft.client.MinecraftClient;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(MinecraftClient.class)
 public class IdentifyStopperPatch {
-    @Inject(method = "scheduleStop", at = @At("HEAD"))
-    private void onStop(CallbackInfo ci) {
-        Thread.dumpStack();
-    }
+	@Shadow
+	private volatile boolean running;
+
+	@Inject(method = "scheduleStop", at = @At("HEAD"))
+	private void onStop(CallbackInfo ci) {
+		if (this.running)
+			Thread.dumpStack();
+	}
 }

21c5ceef0e5a8b3267ec52d73795727c6cf08207
Linnea Gräf <nea@nea.moe>
2026-01-30 18:44:59 +0100

    feat(dev): detect repeat keybindings

--- src/main/kotlin/events/HandledScreenKeyPressedEvent.kt
+++ src/main/kotlin/events/HandledScreenKeyPressedEvent.kt
@@ -3,6 +3,7 @@ package moe.nea.firmament.events
 import org.lwjgl.glfw.GLFW
 import net.minecraft.client.gui.screens.Screen
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen
+import moe.nea.firmament.events.HandledScreenKeyPressedEvent.Companion.internalPollAction
 import moe.nea.firmament.keybindings.GenericInputAction
 import moe.nea.firmament.keybindings.InputModifiers
 import moe.nea.firmament.keybindings.SavedKeyBinding
@@ -14,23 +15,38 @@ sealed interface HandledScreenInputEvent {
 }
 
 data class HandledScreenKeyPressedEvent(
-    override val screen: Screen,
-    override val input: GenericInputAction,
-    override val modifiers: InputModifiers,
-	// TODO: val isRepeat: Boolean,
+	override val screen: Screen,
+	override val input: GenericInputAction,
+	override val modifiers: InputModifiers,
 ) : FirmamentEvent.Cancellable(), HandledScreenInputEvent {
+	val isRepeat: Boolean = internalPollAction() == GLFW.GLFW_REPEAT
 	fun matches(keyBinding: SavedKeyBinding, atLeast: Boolean = false): Boolean {
 		return keyBinding.matches(input, modifiers, atLeast)
 	}
 
 	fun isLeftClick() = input == GenericInputAction.mouse(GLFW.GLFW_MOUSE_BUTTON_LEFT)
-	companion object : FirmamentEventBus<HandledScreenKeyPressedEvent>()
+
+	companion object : FirmamentEventBus<HandledScreenKeyPressedEvent>() {
+		private var lastAction = -1
+
+		/**
+		 * save the exact action last passed to [net.minecraft.client.KeyboardHandler.keyPress]. can be restored using
+		 * [internalPollAction]
+		 */
+		fun internalPushAction(action: Int) {
+			lastAction = action
+		}
+
+		fun internalPollAction(): Int {
+			return lastAction
+		}
+	}
 }
 
 data class HandledScreenKeyReleasedEvent(
-    override val screen: AbstractContainerScreen<*>,
-    override val input: GenericInputAction,
-    override val modifiers: InputModifiers,
+	override val screen: AbstractContainerScreen<*>,
+	override val input: GenericInputAction,
+	override val modifiers: InputModifiers,
 ) : FirmamentEvent.Cancellable(), HandledScreenInputEvent {
 	fun matches(keyBinding: SavedKeyBinding, atLeast: Boolean = false): Boolean {
 		return keyBinding.matches(input, modifiers, atLeast)
